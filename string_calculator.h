486edcf74e3a87bb28ec74b53d43da0763a9acd2
eri2kor <revi.hridya@in.bosch.com>
2025-09-22 23:05:36 +0530

    Update string_calculator.h

--- string_calculator.h
+++ string_calculator.h
@@ -1,13 +1,17 @@
 #pragma once
 #include <string>
 #include <vector>
+#include <utility>
 
 class StringCalculator {
-public:
-    int Add(const std::string& input);
+ public:
+  int add(const std::string& input);
 
-private:
-    std::pair<std::string, std::string> parseDelimiter(const std::string& input) const;
-    std::vector<std::string> split(const std::string& numbers, const std::vector<std::string>& delimiters) const;
-    int toInt(const std::string& token) const;
+ private:
+  std::pair<std::string, std::string> parseDelimiter(
+      const std::string& input) const;
+  std::vector<std::string> split(
+      const std::string& numbers,
+      const std::vector<std::string>& delimiters) const;
+  int toInt(const std::string& token) const;
 };
