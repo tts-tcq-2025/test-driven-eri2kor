80b97611bb8c3ae5c27c613c1190bc2268b9d1a0
eri2kor <revi.hridya@in.bosch.com>
2025-09-22 22:39:39 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -8,4 +8,7 @@ public:
 
 private:
     std::vector<std::string> splitNumbers(const std::string& numbers);
+    std::vector<std::string> extractCustomDelimiters(const std::string& header);
+    std::string buildRegexPattern(const std::vector<std::string>& delimiters);
+    std::vector<std::string> tokenizeNumbers(const std::string& nums, const std::string& pattern);
 };
