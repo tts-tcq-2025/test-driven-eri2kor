75a60481eb8b9eba32d06356825123e9433e4866
eri2kor <revi.hridya@in.bosch.com>
2025-09-22 20:49:43 +0530

    Update and rename string_calculator.h to StringCalculator.h

---
+++ StringCalculator.h
@@ -0,0 +1,12 @@
+#ifndef STRINGCALCULATOR_H
+#define STRINGCALCULATOR_H
+
+#include <string>
+#include <vector>
+
+class StringCalculator {
+public:
+    int add(const std::string& numbers);
+};
+
+#endif
