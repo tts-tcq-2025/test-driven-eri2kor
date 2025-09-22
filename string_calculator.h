b682c78dc74f937b0e7eb825e443353a7114d00a
eri2kor <revi.hridya@in.bosch.com>
2025-09-22 19:27:21 +0530

    Update string_calculator.h

--- string_calculator.h
+++ string_calculator.h
@@ -7,7 +7,9 @@ public:
     int Add(const std::string &numbers);
 
 private:
-    // Pure helper functions
+    // Pure helpers
     static std::vector<std::string> Split(const std::string &str, const std::string &delimiter);
     static std::string DetectDelimiter(const std::string &numbers, size_t &pos);
+    static std::vector<int> ParseNumbers(const std::vector<std::string> &tokens);
+    static int SumNumbers(const std::vector<int> &numbers);
 };
